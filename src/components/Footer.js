bf5554df469f49a588ac1ebf9721310caf131266
Fabio Bauleo <fbbl861@gmail.com>
2023-02-20 06:36:48 +0100

    basic structure added

---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+export default function Footer(){
+    return (
+        <>
+            <footer>
+                <img src="./public/Logo.svg" alt="little lemon logo"/>
+                <h2>Navigation</h2>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#menu">Menu</a></li>
+                    <li><a href="#reservation">Reservation</a></li>
+                    <li><a href="#orderonline">Order online</a></li>
+                    <li><a href="#login">Log in</a></li>
+                </ul>
+                <h2>Contacts</h2>
+                <ul>
+                    <li>Address</li>
+                    <li>E-Mail</li>
+                    <li>Phone Number</li>
+                </ul>
+                <h2>Social Media</h2>
+                <ul>
+                    <li><a href="#facebook">Facebook</a></li>
+                    <li><a href="#instagram">Instagram</a></li>
+                    <li><a href="#tiktok">Tik Tok</a></li>
+                </ul>
+
+            </footer>
+        </>
+    );
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+export default function Nav(){
+    return (
+        <>
+            <nav>
+                <img src="./public/Logo.svg" alt="little lemon logo"/>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#menu">Menu</a></li>
+                    <li><a href="#reservation">Reservation</a></li>
+                    <li><a href="#orderonline">Order online</a></li>
+                    <li><a href="#login">Log in</a></li>
+                </ul>
+            </nav>
+        </>
+    );
+}
