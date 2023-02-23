52ebd20384feb183cb1d19cf6bba8426f4d2fef8
Fabio Bauleo <fbbl861@gmail.com>
2023-02-23 07:01:18 +0100

    CSS first attempt

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ export default function Footer(){
         <>
             <footer>
                 <img src="Logo.svg" alt="little lemon logo"/>
-                <h2>Navigation</h2>
                 <ul>
+                    <li><h2>Navigation</h2></li>
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#menu">Menu</a></li>
@@ -12,14 +12,14 @@ export default function Footer(){
                     <li><a href="#orderonline">Order online</a></li>
                     <li><a href="#login">Log in</a></li>
                 </ul>
-                <h2>Contacts</h2>
                 <ul>
+                    <li><h2>Contacts</h2></li>
                     <li>Address</li>
                     <li>E-Mail</li>
                     <li>Phone Number</li>
                 </ul>
-                <h2>Social Media</h2>
                 <ul>
+                    <li><h2>Social Media</h2></li>
                     <li><a href="#facebook">Facebook</a></li>
                     <li><a href="#instagram">Instagram</a></li>
                     <li><a href="#tiktok">Tik Tok</a></li>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,8 +2,8 @@ export default function Nav(){
     return (
         <>
             <nav>
-                <img src="Logo.svg" alt="little lemon logo"/>
                 <ul>
+                    <li><img src="Logo.svg" alt="little lemon logo"/></li>
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#menu">Menu</a></li>
