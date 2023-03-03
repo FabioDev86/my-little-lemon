2dc2c5b47795f23ef7da809c76511089d2693c95
Fabio Bauleo <fbbl861@gmail.com>
2023-03-03 05:54:22 +0100

    added Link to Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,15 @@
+import {Link} from 'react-router-dom';
+
 export default function Nav(){
     return (
         <>
             <nav>
                 <ul>
                     <li><img src="Logo.svg" alt="little lemon logo"/></li>
-                    <li><a href="#home">Home</a></li>
+                    <li><Link to="/">Home</Link></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#menu">Menu</a></li>
-                    <li><a href="#reservation">Reservation</a></li>
+                    <li><Link to='/reserve-table'>Reservation</Link></li>
                     <li><a href="#orderonline">Order online</a></li>
                     <li><a href="#login">Log in</a></li>
                 </ul>
