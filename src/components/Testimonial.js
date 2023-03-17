dffd7a202414fc43cb5cd4ad25f13c2062761175
Fabio Bauleo <fbbl861@gmail.com>
2023-03-17 06:29:02 +0100

    styled Testimonials

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,9 +1,19 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+
 export default function Testimonial(props){
     return(
-        <>
+        <div className="testimonial">
             <h2>{props.name}</h2>
             <img src={props.source} alt={props.alt}/>
+            <div className='retings'>
+                <FontAwesomeIcon icon={faStar} className="star" />
+                <FontAwesomeIcon icon={faStar} className="star" />
+                <FontAwesomeIcon icon={faStar} className="star" />
+                <FontAwesomeIcon icon={faStar} className="star" />
+                <FontAwesomeIcon icon={faStar} className="star" />
+            </div>
             <p>{props.review}</p>
-        </>
+        </div>
     );
 }
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -2,11 +2,13 @@ import Testimonial from "./Testimonial"
 
 export default function Testimonials(){
     return (
-        <>
+        <div className="testimonials">
             <h1>Testimonials</h1>
-            <Testimonial source='Testimonial1.png' alt='a testimonial' name='Name' review='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nulla leo, placerat dignissim finibus eu, tristique sed metus. Etiam quam nulla, ornare sit amet diam ' />
-            <Testimonial source='Testimonial2.png' alt='a testimonial' name='Name' review='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nulla leo, placerat dignissim finibus eu, tristique sed metus. Etiam quam nulla, ornare sit amet diam ' />
-            <Testimonial source='Testimonial3.png' alt='a testimonial' name='Name' review='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nulla leo, placerat dignissim finibus eu, tristique sed metus. Etiam quam nulla, ornare sit amet diam ' />
-        </>
+            <div className="testimonial-cards">
+                <Testimonial source='Testimonial1.png' alt='a testimonial' name='Name' review='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nulla leo, placerat dignissim finibus eu, tristique sed metus. Etiam quam nulla, ornare sit amet diam ' />
+                <Testimonial source='Testimonial2.png' alt='a testimonial' name='Name' review='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nulla leo, placerat dignissim finibus eu, tristique sed metus. Etiam quam nulla, ornare sit amet diam ' />
+                <Testimonial source='Testimonial3.png' alt='a testimonial' name='Name' review='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nulla leo, placerat dignissim finibus eu, tristique sed metus. Etiam quam nulla, ornare sit amet diam ' />
+            </div>
+        </div>
     );
 }
